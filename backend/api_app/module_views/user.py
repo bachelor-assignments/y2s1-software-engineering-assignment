1a45fa3dede9e59aba694b56251976ed100e7601
Fr0xty <ongjiae8@gmail.com>
2025-10-08 04:48:25 +0800

    get user roles endpoint done

--- backend/api_app/module_views/user.py
+++ backend/api_app/module_views/user.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+from api_app.models import Role
+from ..serializers import RoleSerializer
 
 
 class User(APIView):
@@ -21,5 +23,8 @@ class UserList(APIView):
 
 
 class UserRoles(APIView):
+    # TODO: auth permission required
     def get(self, request: Request):
-        return Response("todo", status=status.HTTP_200_OK)
+        role_list = Role.objects.all()
+        serializer = RoleSerializer(role_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
