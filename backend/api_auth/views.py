7f6159d866de7420abc4ace5772e9ead499a33ae
Fr0xty <ongjiae8@gmail.com>
2025-10-10 07:56:55 +0800

    removed old login endpoint to use simplejwt endpoints

--- backend/api_auth/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.request import Request
-from rest_framework import status
-
-
-class Login(APIView):
-    def post(self, request: Request):
-        return Response("todo", status=status.HTTP_200_OK)
