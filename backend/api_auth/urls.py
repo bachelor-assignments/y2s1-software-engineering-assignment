43cc09a81e638dbfe5e9476acdd91796024b8c79
Fr0xty <ongjiae8@gmail.com>
2025-11-10 20:13:21 +0800

    logout url missing ending slash

--- backend/api_auth/urls.py
+++ backend/api_auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import MyTokenObtainPairView, MyTokenRefreshView, LogoutView
 
 urlpatterns = [
-    path("logout", LogoutView.as_view()),
+    path("logout/", LogoutView.as_view()),
     path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", MyTokenRefreshView.as_view(), name="token_refresh"),
 ]
